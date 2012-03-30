5b5623ef0123f291b8589fb17324ef583d8ec3d6
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-03-30 07:19:13 +0000

    add analysis of poker type

--- QTProject/ServerLayer/GSPlayer.cpp
+++ QTProject/ServerLayer/GSPlayer.cpp
@@ -1,4 +1,5 @@
 #include "GSPlayer.h"
+#include <QtAlgorithms>
 
 GSPlayer::GSPlayer(QTcpSocket* _socket)
 : ISocketInstance(_socket)
@@ -9,9 +10,104 @@ GSPlayer::GSPlayer(QTcpSocket* _socket)
 	mExp = 0;
 	mPlayTime = 0;
 	mIsWaiting = false;
+	mHandPokerType = NONE;
 }
 
 GSPlayer::~GSPlayer()
 {
 
+}
+
+int GSPlayer::CanPush( QList<PokerPtr>& _other )
+{
+
+
+	return 1;
+}
+
+void GSPlayer::reorderPokers2toA( QList<PokerPtr>& _input )
+{
+	if ( _input.isEmpty() )
+		return;
+
+	//QList<PokerPtr> temp = _input.toList();
+	//qSort(temp.begin(), temp.end(), lessThan);
+	//QSet<PokerPtr> res = QSet<PokerPtr>::fromList(temp);
+
+	qSort(_input.begin(), _input.end(), lessThan);
+}
+
+PokerType GSPlayer::analysePokerType( QList<PokerPtr>& _input )
+{
+	if ( _input.isEmpty() )
+		return NONE;
+
+	reorderPokers2toA(_input);
+
+	if ( (_input[0]->mValue == _input[1]->mValue) && (_input[1]->mValue  == _input[2]->mValue) )
+	{
+		return BAOZI;
+	}
+	
+	if ( (_input[0]->mFlower == _input[1]->mFlower) && (_input[1]->mFlower == _input[2]->mFlower) )
+	{
+		if ( (_input[2]->mValue-_input[1]->mValue == 1) && (_input[1]->mValue-_input[0]->mValue == 1) )
+		{
+			return SHUNJIN;
+		}
+		else
+			return JINHUA;
+	}
+	else
+	{
+		if ( (_input[2]->mValue-_input[1]->mValue == 1) && (_input[1]->mValue-_input[0]->mValue == 1) )
+		{
+			return SHUNZI;
+		}
+	}
+
+	if ( _input[2]->mValue == _input[1]->mValue ||
+		_input[2]->mValue==_input[0]->mValue ||
+		_input[0]->mValue==_input[1]->mValue)
+	{
+		return DUIZI;
+	}
+
+	if ( _input[0]->mValue == 2 && _input[1]->mValue == 3 && _input[2]->mValue == 5 )
+	{
+		return SANPAI235;
+	}
+
+	return SANPAI;
+}
+
+bool GSPlayer::lessThan( const PokerPtr &s1, const PokerPtr &s2 )
+{
+	return s1->mValue < s2->mValue;
+}
+
+void GSPlayer::AddPoker( quint32 _id )
+{
+	if ( mHandPoker.size() == MAX_HAND_POKER )
+		return;
+
+	if ( _id > 51 || _id < 0 )
+		return;
+
+	PokerPtr p = PokerPtr(new Poker());
+	p->mValue = _id % 13 + 2;		// +2 是因为整除得到的值是从0开始，而牌面值是从2开始
+	p->mID = _id;
+	p->mFlower = (PokerFlower)(int)(_id/13);
+
+	mHandPoker.append(p);
+
+	if ( mHandPoker.size() == MAX_HAND_POKER )
+	{
+		mHandPokerType = analysePokerType(mHandPoker);
+	}
+}
+
+void GSPlayer::CleanPokers()
+{
+	mHandPoker.clear();
 }
--- QTProject/ServerLayer/GSPlayer.h
+++ QTProject/ServerLayer/GSPlayer.h
@@ -15,6 +15,70 @@
 #include "ISocketInstance.h"
 #include "MyToolkits.h"
 
+#define MAX_HAND_POKER 3
+
+enum PokerType
+{
+	BAOZI=0,	// 豹子 三张点数相同的牌，如AAA 222
+	SHUNJIN,	// 顺金 花色相同的顺子，如果红桃234
+	JINHUA,		// 金花 花色相同，非顺子，如红桃259
+	SHUNZI,		// 顺子 花色不同的顺子，如红桃2黑桃3方片4
+	DUIZI,		// 对子 两张相同的牌，如223
+	SANPAI,		// 散牌 不是任何牌型的牌
+	SANPAI235,	// 散牌235 任意花色的235
+
+	NONE,
+};
+
+enum PokerFlower
+{
+	HEITAO=0,
+	HONGTAO,
+	MEIHUA,
+	FANGPIAN,
+};
+
+class Poker
+{
+public:
+	quint32		mID;		// ID：从2～K～A，0～51，按照黑红梅方顺序
+	quint32		mValue;		// 牌面值： 从2～K～A，0~12；做比大小用
+	PokerFlower	mFlower;	// 花色
+
+	Poker()
+	{
+		mID=mValue=0;
+		mFlower = HEITAO;
+	}
+
+	// for fit hash table
+	Poker &operator=(const Poker &other)
+	{
+		this->mFlower = other.mFlower;
+		this->mID = other.mID;
+		this->mValue = other.mValue;
+		return *this;
+	}
+
+};
+
+typedef QSharedPointer<Poker> PokerPtr;
+
+//////////////////////////////////////////////////////////////////
+/////////////////////// Poker value table //////////////////////
+//////////////////////////////////////////////////////////////////
+//ID		0  1  2  3  4  5  6  7  8   9  10  11  12    Heitao
+//Value     2  3  4  5  6  7  8  9  10  J  Q   K   A
+//ID		13 14 15 16 17 18 19 20 21  22 23  24  25	Hongtao
+//Value     2  3  4  5  6  7  8  9  10  J  Q   K   A
+//ID		26 27 28 29 30 31 32 33 34  35 36  37  38	Meihua
+//Value     2  3  4  5  6  7  8  9  10  J  Q   K   A
+//ID		39 40 41 42 43 44 45 46 47  48 49  50  51	Fangpian
+//Value     2  3  4  5  6  7  8  9  10  J  Q   K   A
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+
+
 class GSPlayer : public ISocketInstance
 {
 public:
@@ -45,10 +109,28 @@ public:
 	// 来源IP
 	PROPERTY(QString, IP);
 
+	void AddPoker(quint32 _id);
+
+	void CleanPokers();
+
+	// 判断别人的牌是否能够压上
+	// return value: 1=true 0=false -1=same
+	int CanPush(QList<PokerPtr>& _other);
 protected:
 private:
 	// 保存手牌
-	QSet<int>	mHandPoker;
+	QList<PokerPtr>	mHandPoker;
+	PokerType mHandPokerType;
+
+private:
+	// 让扑克从小往大排序
+	void reorderPokers2toA(QList<PokerPtr>& _input);
+
+	// 分析手中的牌是哪种牌型
+	PokerType analysePokerType( QList<PokerPtr>& _input );
+
+	static bool lessThan(const PokerPtr &s1, const PokerPtr &s2);
+
 };
 
 typedef QSharedPointer<GSPlayer> GSPlayerPtr;
