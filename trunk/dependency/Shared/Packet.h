1a4d1db06b41892ff9d139efd8ed0f2f422c3938
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-06-07 09:40:18 +0000

    fix bug: readAll bytes cause 2 packets connected

--- trunk/dependency/Shared/Packet.h
+++ trunk/dependency/Shared/Packet.h
@@ -22,7 +22,7 @@
 
 /////////////////////////////// Packet Header Format //////////////////////////////////////////////////
 // | byte0 | byte1 | byte2 | byte3 | byte4 | byte5 | byte6 | byte7 | byte8 | byte9 | byte10 | byte11 | 
-// |		Message Size		   |			Token	   |			Message Type		 |
+// |		Message Size		   |			Token			   |			Message Type		 |
 /////////////////////////////// Packet Header Format //////////////////////////////////////////////////
 #include <QByteArray>
 #include <QDataStream>
