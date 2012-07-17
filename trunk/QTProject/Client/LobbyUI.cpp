c4cd710f5b5f158dd3625d1e487d819945111728
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-07-17 07:00:42 +0000

    add table layout as a child of lobby ui

--- trunk/QTProject/Client/LobbyUI.cpp
+++ trunk/QTProject/Client/LobbyUI.cpp
@@ -14,7 +14,7 @@ LobbyUI::~LobbyUI()
 void LobbyUI::Init()
 {
 	QUiLoader uiLoader;
-	QString fileStr(":/Client/table.ui");
+	QString fileStr(":/Client/lobby.ui");
 	QFile uiFile(fileStr);
 	mMainWidget = uiLoader.load(&uiFile);
 	if ( mMainWidget )
@@ -22,10 +22,34 @@ void LobbyUI::Init()
 		mMainWidget->setWindowFlags(Qt::FramelessWindowHint);
 	}
 
+	initTables();
+
 	regConnections();
 }
 
 void LobbyUI::regConnections()
 {
+	connect(mMainWidget->findChild<QPushButton*>("btn_quit"), SIGNAL(clicked()), this, SIGNAL(SiQuit()));
+}
 
+void LobbyUI::initTables()
+{
+	QUiLoader uiLoader;
+	QString fileStr(":/Client/table.ui");
+	QFile uiFile(fileStr);
+	QWidget* tableWidget = uiLoader.load(&uiFile);
+	if ( tableWidget )
+	{
+		QScrollArea* tableScroll = mMainWidget->findChild<QScrollArea*>("tableList");
+		tableWidget->setParent(tableScroll->widget());
+	}
+
+	QFile uiFile2(fileStr);
+	QWidget* tableWidget2 = uiLoader.load(&uiFile2);
+	if ( tableWidget2 )
+	{
+		QScrollArea* tableScroll = mMainWidget->findChild<QScrollArea*>("tableList");
+		tableWidget2->setParent(tableScroll->widget());
+		tableWidget2->move(100, 0);
+	}
 }
