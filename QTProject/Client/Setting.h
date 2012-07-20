1065fe2c69a6c74d98bdd22a4cbbb47cf04788c2
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-07-20 07:03:10 +0000

    add setting module for client

---
+++ QTProject/Client/Setting.h
@@ -0,0 +1,37 @@
+/********************************************************************
+	created:	 2012/07/20
+	created:	 20:7:2012   14:54
+	file name:	 Setting.h
+	author:		 PangShuo
+	description:
+*********************************************************************/
+
+
+#ifndef Setting_h__
+#define Setting_h__
+
+#include <QString>
+#include "MyToolkits.h"
+
+class QSettings;
+class Setting : public Singleton<Setting>
+{
+public:
+	Setting();
+	~Setting();
+	void Init(QString _fileName);
+
+	PROPERTY(QString, IP);
+	PROPERTY(uint, Port);
+	PROPERTY(QString, LoginADURL);
+	PROPERTY(QString, LoginNewsURL);
+
+
+protected:
+private:
+	QSettings* mSettings;
+};
+
+#define SETTINGS Setting::GetSingleton()
+
+#endif // Setting_h__
