6faec44cc7eb9205028061e5b0d512682d872045
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-07-25 09:26:43 +0000

    user QGraphicsView and QScene instead of QScrollArea as been tableList

--- QTProject/Client/Table.cpp
+++ QTProject/Client/Table.cpp
@@ -16,7 +16,7 @@ Table::~Table()
 void Table::Init()
 {
 	QLabel* tableID = mWidget->findChild<QLabel*>("tableID");
-	tableID->setText(QString("%1").arg(mID));
+	tableID->setText(QString("%1").arg(mID+1));
 }
 
 void Table::StSit( quint32 _tableID, quint32 _seatID, quint32 _accountID )
