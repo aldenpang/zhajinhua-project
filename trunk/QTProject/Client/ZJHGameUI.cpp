1542a917995360cc4c6feb141f958f8c98c04813
pangshuo1981@gmail.com <pangshuo1981@gmail.com@d8f4ed1e-bbb0-a400-853e-1b27d4c80825>
2012-08-10 07:09:49 +0000

    add shuffle and distribute animation

--- trunk/QTProject/Client/ZJHGameUI.cpp
+++ trunk/QTProject/Client/ZJHGameUI.cpp
@@ -3,10 +3,10 @@
 #include "PokerItem.h"
 
 QPoint gTableCenter = QPoint(360, 220);
-QPoint gPokerPos0 = QPoint(420, 510);
-QPoint gPokerPos1 = QPoint(620, 350);
-QPoint gPokerPos2 = QPoint(440, 180);
-QPoint gPokerPos3 = QPoint(300, 350);
+QPoint gPokerPos[MAX_PLAYER][MAX_PLAYER] = {{QPoint(300, 360), QPoint(550, 220), QPoint(300, 60), QPoint(110, 220)}, 
+											{QPoint(550, 220), QPoint(300, 60), QPoint(110, 220), QPoint(300, 360)}, 
+											{QPoint(300, 60), QPoint(110, 220), QPoint(300, 360), QPoint(550, 220)}, 
+											{QPoint(110, 220), QPoint(300, 360), QPoint(550, 220), QPoint(300, 60)}};
 quint32 gPokerGap = 20;
 
 ZJHGameUI::ZJHGameUI()
@@ -32,19 +32,34 @@ void ZJHGameUI::Init()
 
 	regConnections();
 
-	mTimer.start();
+	mTimer.start(5000);
 
 	QGraphicsView* view = mMainWidget->findChild<QGraphicsView*>("graphicsView");
 	mScene = new QGraphicsScene(0, 0, 
 		view->size().width()-view->verticalScrollBar()->size().width(),
 		view->size().height()-view->horizontalScrollBar()->size().height());
+	//mScene->setBackgroundBrush(Qt::blue);
 	view->setScene(mScene);
 
-	PokerItem* item = new PokerItem(0);
-	item->toBack();
-	//item->moveBy(gTableCenter.x(), gTableCenter.y());
-	mScene->addItem(item);
+	mShuffleLabel = new QLabel(mMainWidget);
+	mShuffleLabel->move(gTableCenter+QPoint(50, 100));
+	mShuffleAni = new QMovie(":/Images/Media/shuffle.gif");
+	mShuffleLabel->setMovie(mShuffleAni);
+	HideShuffleAni();
 
+	int amount = MAX_HAND_POKER*MAX_PLAYER;
+
+	for ( int i = 0; i<amount; i++ )
+	{
+		PokerItem* item = new PokerItem(0);
+		item->setZValue(i);
+		item->setPos(gTableCenter);
+		item->ToBack();
+		mScene->addItem(item);
+		mPokers.push_back(item);
+	}
+
+	ShowDistributeAni(0);
 }
 
 void ZJHGameUI::regConnections()
@@ -60,4 +75,47 @@ void ZJHGameUI::stUpdate()
 	//{
 	//	poker->move(poker->pos().x()+1, poker->pos().y());
 	//}
+
+	//ShowPreShuffleAni();
+}
+
+void ZJHGameUI::ShowShuffleAni()
+{
+	mShuffleLabel->show();
+	mShuffleAni->start();
+}
+
+void ZJHGameUI::HideShuffleAni()
+{
+	mShuffleLabel->hide();
+	mShuffleAni->stop();
+}
+
+void ZJHGameUI::ShowDistributeAni( quint32 _dealerIdx, quint32 _absentIdx1/*=-1*/, quint32 _absentIdx2/*=-1*/ )
+{
+	if ( _dealerIdx >= MAX_PLAYER || _dealerIdx < 0 )
+	 return;
+
+	QPoint* pos = gPokerPos[_dealerIdx];
+	int posIdx = 0;
+	int gap = 0;
+	for ( int i = 0; i<mPokers.size(); i++, posIdx++ )
+	{
+		if ( i != 0 && i %  MAX_PLAYER == 0 )
+		{
+			gap++;
+			posIdx = 0;
+		}
+
+		mPokers[i]->Move(150*i, 1000, QPoint(pos[posIdx].x()+gPokerGap*gap, pos[posIdx].y()));
+	}
+
+}
+
+void ZJHGameUI::ShowPreShuffleAni()
+{
+	for ( int i = 0; i<mPokers.size(); i++ )
+	{
+		mPokers[i]->Move(50*i, 1000, gTableCenter);
+	}
 }
--- trunk/QTProject/Client/package.bat
+++ trunk/QTProject/Client/package.bat
@@ -1,3 +1,5 @@
 rcc -binary client.qrc -o media.dat
 
 copy media.dat ..\Bin
+
+echo package and copy meida.dat successful!
